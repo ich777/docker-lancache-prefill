83f23b5055d26a71f2493a62ccef2c2dc3707e56
ich777 <christophhummer@gmail.com>
2022-11-16 19:19:22 +0100

    „cron/steam_prefill.sh“ hinzufügen

---
+++ cron/steam_prefill.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "${ENABLE_STEAM}" == "true" ]; then
+  if [ "$(pidof SteamPrefill)" ]; then
+    echo "[$(date +%F)] SteamPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/steam_prefill.log
+  else
+    echo "[$(date +%F)] Starting SteamPrefill" >> ${DATA_DIR}/logs/steam_prefill.log
+    ${DATA_DIR}/SteamPrefill/SteamPrefill prefill --no-ansi ${PREFILL_PARAMS_STEAM} >> ${DATA_DIR}/logs/steam_prefill.log
+  fi
+fi
