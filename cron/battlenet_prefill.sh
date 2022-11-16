89856e518a579d9ce531b1ed440b1c418a435b60
ich777 <christophhummer@gmail.com>
2022-11-16 19:20:23 +0100

    „cron/battlenet_prefill.sh“ hinzufügen

---
+++ cron/battlenet_prefill.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "${ENABLE_BN}" == "true" ]; then
+  if [ "$(pidof BattleNetPrefill)" ]; then
+    echo "[$(date +%F)] BattleNetPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/battlenet_prefill.log
+  else
+    echo "[$(date +%F)] Starting BattleNetPrefill" >> ${DATA_DIR}/logs/battlenet_prefill.log
+    ${DATA_DIR}/BattleNetPrefill/BattleNetPrefill prefill ${PREFILL_PARAMS_BN} >> ${DATA_DIR}/logs/battlenet_prefill.log
+  fi
+fi
