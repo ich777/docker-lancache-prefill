c2798718fd4b8fe518a9a51dae1220a7595054b4
ich777 <christophhummer@gmail.com>
2023-02-14 11:15:45 +0100

    „cron/epic_prefill.sh“ ändern

--- cron/epic_prefill.sh
+++ cron/epic_prefill.sh
@@ -5,6 +5,6 @@ if [ "${ENABLE_EPIC}" == "true" ]; then
     echo "[$(date +%F)] EpicPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/epic_prefill.log
   else
     echo "[$(date +%F)] Starting EpicPrefill" >> ${DATA_DIR}/logs/epic_prefill.log
-    ${DATA_DIR}/EpicPrefill/EpicPrefill prefill --no-ansi ${PREFILL_PARAMS_STEAM} >> ${DATA_DIR}/logs/epic_prefill.log
+    ${DATA_DIR}/EpicPrefill/EpicPrefill prefill --no-ansi ${PREFILL_PARAMS_EPIC} >> ${DATA_DIR}/logs/epic_prefill.log
   fi
 fi
