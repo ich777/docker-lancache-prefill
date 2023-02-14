e764679e825a392881a213f198120ca0d6dff7d9
Christoph Hummer <christophhummer@gmail.com>
2023-02-14 08:51:13 +0100

    Update global_prefill.sh

--- cron/global_prefill.sh
+++ cron/global_prefill.sh
@@ -8,6 +8,14 @@ if [ "${ENABLE_BN}" == "true" ]; then
     ${DATA_DIR}/BattleNetPrefill/BattleNetPrefill prefill ${PREFILL_PARAMS_BN} >> ${DATA_DIR}/logs/battlenet_prefill.log
   fi
 fi
+if [ "${ENABLE_EPIC}" == "true" ]; then
+  if [ "$(pidof EpicPrefill)" ]; then
+    echo "[$(date +%F)] EpicPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/epic_prefill.log
+  else
+    echo "[$(date +%F)] Starting EpicPrefill" >> ${DATA_DIR}/logs/epic_prefill.log
+    ${DATA_DIR}/EpicPrefill/EpicPrefill prefill --no-ansi ${PREFILL_PARAMS_STEAM} >> ${DATA_DIR}/logs/epic_prefill.log
+  fi
+fi
 if [ "${ENABLE_STEAM}" == "true" ]; then
   if [ "$(pidof SteamPrefill)" ]; then
     echo "[$(date +%F)] SteamPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/steam_prefill.log
