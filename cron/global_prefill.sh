8c4da393c6b014cd709418844b05224fefa85c4e
Joly0 <julian0906@hotmail.de>
2023-02-14 10:08:55 +0100

    Fixed typos

--- cron/global_prefill.sh
+++ cron/global_prefill.sh
@@ -13,7 +13,7 @@ if [ "${ENABLE_EPIC}" == "true" ]; then
     echo "[$(date +%F)] EpicPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/epic_prefill.log
   else
     echo "[$(date +%F)] Starting EpicPrefill" >> ${DATA_DIR}/logs/epic_prefill.log
-    ${DATA_DIR}/EpicPrefill/EpicPrefill prefill --no-ansi ${PREFILL_PARAMS_STEAM} >> ${DATA_DIR}/logs/epic_prefill.log
+    ${DATA_DIR}/EpicPrefill/EpicPrefill prefill --no-ansi ${PREFILL_PARAMS_EPIC} >> ${DATA_DIR}/logs/epic_prefill.log
   fi
 fi
 if [ "${ENABLE_STEAM}" == "true" ]; then
