5aa73a9e28a13bc473d12c54a84d437348bd3590
ich777 <christophhummer@gmail.com>
2023-05-30 13:37:11 +0200

    „cron/battlenet_prefill.sh“ ändern

--- cron/battlenet_prefill.sh
+++ cron/battlenet_prefill.sh
@@ -5,6 +5,6 @@ if [ "${ENABLE_BN}" == "true" ]; then
     echo "[$(date +%F)] BattleNetPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/battlenet_prefill.log
   else
     echo "[$(date +%F)] Starting BattleNetPrefill" >> ${DATA_DIR}/logs/battlenet_prefill.log
-    ${DATA_DIR}/BattleNetPrefill/BattleNetPrefill prefill --no-ansi ${PREFILL_PARAMS_BN} >> ${DATA_DIR}/logs/battlenet_prefill.log
+    ${DATA_DIR}/BattleNetPrefill/BattleNetPrefill prefill --no-ansi >> ${DATA_DIR}/logs/battlenet_prefill.log
   fi
 fi
