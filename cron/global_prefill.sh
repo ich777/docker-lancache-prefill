2a756ba945fd429aad87eff2713ac7ba2734c389
Christoph Hummer <christophhummer@gmail.com>
2024-09-10 22:47:08 +0200

    cron/global_prefill.sh aktualisiert

--- cron/global_prefill.sh
+++ cron/global_prefill.sh
@@ -5,7 +5,7 @@ if [ "${ENABLE_BN}" == "true" ]; then
     echo "[$(date +%F)] BattleNetPrefill already running, aborting schedule!" >> ${DATA_DIR}/logs/battlenet_prefill.log
   else
     echo "[$(date +%F)] Starting BattleNetPrefill" >> ${DATA_DIR}/logs/battlenet_prefill.log
-    ${DATA_DIR}/BattleNetPrefill/BattleNetPrefill prefill ${PREFILL_PARAMS_BN} >> ${DATA_DIR}/logs/battlenet_prefill.log
+    ${DATA_DIR}/BattleNetPrefill/BattleNetPrefill prefill --no-ansi ${PREFILL_PARAMS_BN} >> ${DATA_DIR}/logs/battlenet_prefill.log
   fi
 fi
 if [ "${ENABLE_EPIC}" == "true" ]; then
